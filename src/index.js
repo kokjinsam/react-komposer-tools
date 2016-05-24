1f82e3a8a7bd004add4c7701b704729663aef430
skaxwng <samkj.ks@gmail.com>
2016-05-24 12:58:44 -0500

    Removed react-komposer-query

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import composeWithRedux from 'react-komposer-redux';
-import composeWithQueries from 'react-komposer-queries';
+import composeWithQuery from 'react-komposer-query';
 import composeWatchQuery from 'react-komposer-watchQuery';
 
 export {
   composeWithRedux,
-  composeWithQueries,
+  composeWithQuery,
   composeWatchQuery,
 };
