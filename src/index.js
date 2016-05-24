defce645c8fa276ec589a4fe67868a7c9528ca60
skaxwng <samkj.ks@gmail.com>
2016-05-24 12:50:04 -0500

    v0.0.1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import composeWithRedux from 'react-komposer-redux';
+import composeWithQueries from 'react-komposer-queries';
+import composeWatchQuery from 'react-komposer-watchQuery';
+
+export {
+  composeWithRedux,
+  composeWithQueries,
+  composeWatchQuery,
+};
